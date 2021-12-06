86627bd54945995f043b46716e5828af245673ae
Yizong <liyizong78@gmail.com>
2021-12-06 16:15:36 -0500

    Completed lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,8 +81,16 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        }
+
+        if (A.rest == null) {
+            A.rest = B;
+        } else {
+            dcatenate(A.rest, B);
+        }
+        return A;
     }
 
     /**
@@ -90,8 +98,11 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        }
+
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 
